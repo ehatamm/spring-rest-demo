75a3411e06378f2a31e6b084ad0bb2ae1da57817
Maaonlapik <tanel.ehatamm@ttu.ee>
2019-09-11 17:55:43 +0300

    spring-rest-demo-global-exc-handling

---
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -0,0 +1,51 @@
+package com.luv2code.springdemo.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springdemo.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+	
+	private	List<Student> students;
+	
+	// define @PostConsrtuct to load the data only once
+	
+	@PostConstruct
+	public void loadData() {
+		
+		students = new ArrayList<>();
+		students.add(new Student("Tanel", "Ehatamm"));
+		students.add(new Student("Martin", "Muhv"));
+		students.add(new Student("Kaspar", "Kaasik"));
+	}
+	
+	@GetMapping("/students") 
+	public List<Student> getStudents() {
+		return students;
+		
+	}
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		if (students.size() < studentId || studentId < 0) {
+			throw new StudentNotFoundException("Student not found - " + studentId);
+		}
+		
+		return students.get(studentId);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>Spring REST demo</title>
+</head>
+
+<body>
+	<h3>Spring REST DEMO</h3>
+	<hr>
+	
+	<a href="${pageContext.request.contextPath}/test/hello">Hello</a>
+	<br>
+	<a href="${pageContext.request.contextPath}/api/students">Students</a>
+	
+</body>
+
+</html>
